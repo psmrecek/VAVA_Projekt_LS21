4c6f2c7429883464952c055d08b3ec7939902340
Peter Smrecek <petersmrecek@gmail.com>
2021-04-22 16:07:24 +0200

    Added add element methods for lists

--- src/main/java/sk/stu/fiit/user/LeagueOrganizer.java
+++ src/main/java/sk/stu/fiit/user/LeagueOrganizer.java
@@ -3,7 +3,6 @@ package sk.stu.fiit.user;
 import java.util.ArrayList;
 import java.util.Date;
 import sk.stu.fiit.league.League;
-import sk.stu.fiit.league.LeagueCharacteristics;
 import sk.stu.fiit.team.TeamApplicationForLeague;
 
 /**
@@ -15,15 +14,20 @@ import sk.stu.fiit.team.TeamApplicationForLeague;
  */
 public class LeagueOrganizer extends User {
 
-	private String nickname;
-	private ArrayList<League> leagueList;
-	private ArrayList<TeamApplicationForLeague> teamApplicationList;
-	private ArrayList<LeagueCharacteristics> leagueCharacteristics;
+    private String nickname;
+    private ArrayList<League> leagueList;
+    private ArrayList<TeamApplicationForLeague> teamApplicationList;
 
     public LeagueOrganizer(String email, String name, String surrname, String nickname, String password, Date bornAt) {
         super(email, name, surrname, nickname, password, bornAt);
     }
         
-        
+    public void addLeague(League league){
+        leagueList.add(league);
+    }
+
+    public void addApplication(TeamApplicationForLeague tafl){
+        teamApplicationList.add(tafl);
+    }
 	
 }
